4139d8b55fbc6584120d8898d1be82c6918d1f85
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-05-09 21:21:10 +0900

    Enable null for "todo_id" column in items

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_113823) do
+ActiveRecord::Schema.define(version: 2021_05_09_121419) do
 
   create_table "items", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.string "name"
     t.boolean "done"
-    t.bigint "todo_id", null: false
+    t.bigint "todo_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["todo_id"], name: "index_items_on_todo_id"
